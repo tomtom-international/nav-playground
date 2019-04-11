ed222ae324fd631c1d5a470791b2dad704bf0435
Eugene Davis <eugene.davis@tomtom.com>
2019-04-11 12:48:13 +0200

    Separated norm and abs path tests

--- tests/test_nav_playground.py
+++ tests/test_nav_playground.py
@@ -13,24 +13,20 @@ import hypothesis.strategies as strat
 
 
 class TestFileNormFuzzing:
-    @given(
-        file_path=strat.text(
-            min_size=1, alphabet=strat.characters(blacklist_categories=("C"))
-        )
-    )
+    @given(file_path=strat.text(min_size=1))
     @example(file_path=" ")
     @example(file_path="//")
-    def test_different_file_path(self, file_path):
-        """
-        Tests that secret keys, whitespace in paths, and hidden files are all handled correctly
-        """
-
-        abs_path_1 = abspath(file_path)
-        abs_path_2 = abspath(abs_path_1)
-
-        assert abs_path_1 == abs_path_2
-
+    def test_normpath(self, file_path):
         norm_path_1 = normpath(file_path)
         norm_path_2 = normpath(norm_path_1)
 
         assert norm_path_1 == norm_path_2
+
+    @given(file_path=strat.text(min_size=1))
+    @example(file_path=" ")
+    @example(file_path="//")
+    def test_abspath(self, file_path):
+        abs_path_1 = abspath(file_path)
+        abs_path_2 = abspath(abs_path_1)
+
+        assert abs_path_1 == abs_path_2
